c7e55abb79dbdbd67f30d1e64547d58dd95c0338
shroommm <khacnguyenquan@gmail.com>
2025-05-16 15:20:42 +0700

    feat: change tab's title

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Game from "./Game.tsx"
+import {Helmet} from "react-helmet";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <>
+    <Helmet>
+      <title>Crossing Chicken</title>
+    </Helmet>
     <Game />
+    </>
   </StrictMode>,
 )
